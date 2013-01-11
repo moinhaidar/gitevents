3d823f0cbbeeed47659a46549dacf6052d0a5ee9
Moin Haidar <moinhaidar@gmail.com>
2013-01-11 18:38:49 +0530

    ajaxifying the same

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,15 @@
 class HomeController < ApplicationController
   
+  respond_to :js, :json, :html, :xml
+  
   def welcome
     
   end
   
   def fetch_events
-    if request.xhr?
-      @github_id = 'Blah'
-      @events = []
-    else
-      @github_id = params[:github_id]
-      @events = TestFetcher.fetch_events(@github_id)
-    end
+    @github_id = params[:github_id]
+    @events = TestFetcher.fetch_events(@github_id)
+    respond_with(@events)
   end
   
 end
